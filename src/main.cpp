1005348e29749d2a390792d93748f0afd9a3b142
Shreyansh Gupta <shreyanshgupta131977@gmail.com>
2026-02-09 09:39:17 +0530

    ldr

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,21 @@
 #include <Arduino.h>
+int LDR_PIN = A0;
 void setup() {
-// write your initialization code here
+    Serial.begin(9600);
+    pinMode(LDR_PIN, INPUT);
+
 }
 
 void loop() {
-// write your code here
+    int ldrRaw = analogRead(LDR_PIN);
+    int lightPercent=map(ldrRaw, 0,1023, 0, 100);
+
+    Serial.print("ldrRaw:");
+    Serial.println(ldrRaw);
+    Serial.print("lightPercent:");
+    Serial.print(lightPercent);
+    Serial.println("%");
+    delay(300);
+
+
 }
